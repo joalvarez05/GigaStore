6a57b0f9b6c76a87edb4c6279de035f8194d1ba8
José Alvarez Domínguez <josealvarezdominguez00@gmail.com>
2024-09-17 13:32:33 -0300

    primer archivo js, añadiendo botones con estado 'me gusta' y 'no me gusta'

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+const meGusta = document.getElementById("btnMeGusta");
+// Añade el manejador de eventos
+meGusta.addEventListener("click", () => {
+  // Alterna la clase 'liked'
+  meGusta.classList.toggle("liked");
+});
+
+const meGusta1 = document.getElementById("btnMeGusta1");
+// Añade el manejador de eventos
+meGusta1.addEventListener("click", () => {
+  // Alterna la clase 'liked'
+  meGusta1.classList.toggle("liked");
+});
+
+const meGusta2 = document.getElementById("btnMeGusta2");
+// Añade el manejador de eventos
+meGusta2.addEventListener("click", () => {
+  // Alterna la clase 'liked'
+  meGusta2.classList.toggle("liked");
+});
+
+const noMeGusta = document.getElementById("btnNoMeGusta");
+// Añade el manejador de eventos
+noMeGusta.addEventListener("click", () => {
+  // Alterna la clase 'liked'
+  noMeGusta.classList.toggle("unliked");
+});
+const noMeGusta1 = document.getElementById("btnNoMeGusta1");
+// Añade el manejador de eventos
+noMeGusta1.addEventListener("click", () => {
+  // Alterna la clase 'liked'
+  noMeGusta1.classList.toggle("unliked");
+});
+const noMeGusta2 = document.getElementById("btnNoMeGusta2");
+// Añade el manejador de eventos
+noMeGusta2.addEventListener("click", () => {
+  // Alterna la clase 'liked'
+  noMeGusta2.classList.toggle("unliked");
+});
